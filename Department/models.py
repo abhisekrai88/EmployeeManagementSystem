737a46b06e36a88f98b6177d69b4f43f103a5b77
abhisekrai88 <kesihba.iar@gmail.com>
2021-01-26 20:46:47 +0800

    created department modes, serializers and views

--- Department/models.py
+++ Department/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    department_ID = models.AutoField("Department ID", primary_key=True)
+    department_name = models.CharField("Department Name", max_length=30)
+
+    def __str__(self):
+        return self.department_name
--- Department/views.py
+++ Department/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Department
+from .serializers import DepartmentSerializer
 
 # Create your views here.
+
+class DepartmentAPIView(generics.ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializers = [DepartmentSerializer]
